ad2ed20d1b4071c6cea4243a4c4b334d10c66108
Per Johansson <per@johansson.name>
2014-12-06 22:24:30 +0200

    Promise and messaging.

--- chrome/content/dcc-seamonkey-settings-adapter.js
+++ chrome/content/dcc-seamonkey-settings-adapter.js
@@ -4,12 +4,41 @@
 
 const SettingsAdapter = function() {
 //    self.port.on("showSettings", DirectCurrencySettings.showSettings);
+    const showSettings = function(aData) {
+        var promise = new Promise(
+            function(resolve, reject) {
+                if (DirectCurrencySettings)
+                    resolve(DirectCurrencySettings);
+                else
+                    reject("NOK");
+            }
+        );
+        promise.then(
+            function(dcs) {
+                console.error("Promise fulfilled dcs " + dcs + " aData " +  aData);
+                dcs.showSettings(aData)
+            },
+            function (err) {
+                console.error("then "  + err);
+            }
+        );
+        promise.catch(
+            function (err) {
+                console.error("catch " + err);
+            }
+        );
+    };
     const receiveMessage = function(event) {
-        console.error("receiveMessage " + event.data);
-        DirectCurrencySettings.showSettings(event.data);
+        console.error("SettingsAdapter receiveMessage " + event.data
+            + " conversionQuotes " + event.data.conversionQuotes);
+        // DirectCurrencySettings.showSettings(event.data)
+        if (event.data.conversionQuotes !== undefined)
+            showSettings(event.data);
     };
     window.addEventListener("message", receiveMessage, false);
-    console.error("SettingsAdapter");
+    // Send message when created
+    // otherWindow.postMessage("SettingsAdapter created", "*");
+    console.error("SettingsAdapter created");
     return {
         save: function (contentScriptParams) {
             self.port.emit("saveSettings", contentScriptParams);
